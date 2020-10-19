90e6e9346aee6aeade1640e506396e60f699676c
Rafael <rafael-vvianaa@hotmail.com>
2020-10-18 22:31:43 -0300

    Adição parcial do css e templates da lading page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import './styles/global.css';
 
+import './styles/global.css';
+import './styles/pages/lading.css';
+
+import logoImg from './imagens/Logo.svg'
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div id="page-lading">
+     <div className="content-wrapper">
+       <img src={logoImg} alt="Happy"></img>
+       <main>
+         <h1>Leve felicidade para o mundo</h1>
+         <p>Visite orfanatos e mude o dia de mutas crianças.</p>
+       </main>
+
+       <div className="location">
+         <strong>Aracaju</strong>
+         <span>Sergipe</span>
+       </div>
+
+       <a href=""className='enter-app'>
+         >
+       </a>
+     </div>
+    
     </div>
   );
 }
